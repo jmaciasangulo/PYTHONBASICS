9902254efa6e8088fc3c4d6ab38238a632900265
jesusmaciasangulo <jesusalejandromaciasangulo@gmail.com>
2026-03-28 18:03:37 -0600

    Se crea la estructura lógica para la navegación del usuario.

--- LEER Y EDITAR ARCHIVOS/Gestor_de_gastos.py
+++ LEER Y EDITAR ARCHIVOS/Gestor_de_gastos.py
@@ -10,4 +10,27 @@ operaciones = {
 }
 #----------------------------FUNCIONES---------------------------------------
 #----------------------------UNIDAD LOGICA-----------------------------------
-print("GESTOR DE GASTOS PERSONALES")
+
+print("GESTOR DE GASTOS PERSONALES\n")
+while True:
+    print("Usted puede hacer lo siguiente:\n")
+
+    for operacion in operaciones:
+        print(f"-{operacion.capitalize()}")
+
+    operacion = input("\n¿Que desea hacer con su gestor?: ").lower()
+    while operacion not in operaciones:
+        print("Opcion invalida, vuelva a intentar...")
+        operacion = input("¿Que desea hacer con su gestor?: ").lower()
+
+    operaciones[operacion]()
+
+    repetir = input("Desea volver a trabajar con su gestor? [SI/NO]: ").lower()
+    while repetir not in ["si", "no"]:
+        print("Opcion invalida, vuelva a intentar...")
+        repetir = input("Desea volver a trabajar con su gestor? [SI/NO]: ").lower()
+    if repetir == "si":
+        continue
+    else:
+        break
+
