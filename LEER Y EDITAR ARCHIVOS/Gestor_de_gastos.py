426be4130822c298070f734e7a7bb2aacbb821bf
jesusmaciasangulo <jesusalejandromaciasangulo@gmail.com>
2026-04-02 14:03:18 -0600

    Se implementa función. una vez con el filtro seleccionado (para esta función es listar por categoria) busca en el diccionario de lista de tareas, dentro busca en el diccionario de cada tarea la clasificación con la que está guardada, si coincide, se imprime, en su defecto no. Si ninguna tarea coincide, aparece en pantalla el mensaje de que no hay tareas registradas en esa categoria.

--- LEER Y EDITAR ARCHIVOS/Gestor_de_gastos.py
+++ LEER Y EDITAR ARCHIVOS/Gestor_de_gastos.py
@@ -178,6 +178,30 @@ def escoge_listar_gastos():
 
     listar_gastos[filtrar]()
 
+def listar_por_categoria():
+    print("Las categorías disponibles para consulta son:")
+    for categoria in categorias_de_gastos:
+        print(f"-{categoria}.")
+
+    categoria = input("Que categoria desea ver?: ")
+
+    haygastosencategoria = False
+
+    while categoria not in categorias_de_gastos:
+        print("Opcción invalida, vuelva a intentar...")
+        categoria = input("Que categoria desea ver?: ")
+
+    for id_gasto in diccionarios["lista_de_gastos"]:
+        if diccionarios["lista_de_gastos"][id_gasto]["categoria"] == categoria:
+            for clave, valor in diccionarios["lista_de_gastos"][id_gasto].items():
+                print(f"{clave.capitalize()}: {valor}")
+                haygastosencategoria = True
+
+    if not haygastosencategoria:
+        print("No hay gastos registrados con esta categoria.")
+
+
+
 def repetir_bucle(respuesta):
     while respuesta not in ["si", "no"]:
         print("Opcion invalida, vuelva a intentar...")
