6b47877bb958e8eb4da182712a206ade61eaf2f9
jesusmaciasangulo <jesusalejandromaciasangulo@gmail.com>
2026-04-20 18:06:17 -0600

    Se implementa función. Su propósito es retirar dinero. Primero válida que la cantidad sea positiva, después, si la cantidad contiene más de dos decimales, le dice al usuario que solo se utilizarán los primeros 2 decimales, y trunca la cantidad a 2 decimales. Después cambia el balance, muestra en pantalla de cuanto fue el retiro, y como quedó su cuenta después de esa operación.

--- CLASES/Cuenta_de_Banco.py
+++ CLASES/Cuenta_de_Banco.py
@@ -35,6 +35,27 @@ class CuentaDeBanco:
         print(f"Balance actual: {self.balance} USD")
 
     def retirar(self):
+        while True:
+            try:
+                monto = Decimal(input("\nEscriba la cantidad a retirar: "))
+                if monto <= 0:
+                    print("Deposito invalido, escriba solo números positivos")
+                    continue
+                str_monto = str(monto)
+                if "." in str_monto:
+                    decimales = str_monto.split(".")[1]
+                    if len(decimales) > 2:
+                        print("El programa solo guardará los primeros 2 decimales")
+                        monto = monto.quantize(Decimal("0.01"), rounding=ROUND_DOWN)
+                break
+            except InvalidOperation:
+                print("Digite en números la cantidad a depositar.")
+
+        self.balance -= monto
+        print(f"Retiro por la cantidad de: {monto} USD")
+        print(f"Balance actual: {self.balance} USD")
+
+
 
     def calcular_intereses(self):
 
