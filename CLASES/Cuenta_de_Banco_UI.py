e7a34a17f98bf38384fc809f4184f7b3f8c99bd7
jesusmaciasangulo <jesusalejandromaciasangulo@gmail.com>
2026-04-23 19:51:17 -0600

    APARTADO PARA LA CLASE USUARIO

--- CLASES/Cuenta_de_Banco_UI.py
+++ CLASES/Cuenta_de_Banco_UI.py
@@ -1,5 +1,7 @@
 from colorama import init, Fore
 init(autoreset=True)
+
+
 operaciones = {
     "CONSULTAR ESTADO DE CUENTA",
     "RETIRO DE EFECTIVO",
@@ -8,19 +10,34 @@ operaciones = {
     "VER HISTORIAL DE OPERACIONES"
 }
 
-def validar_respuesta(respuesta, diccionario):
-    if respuesta not in diccionario:
-        return False
-    else:
-        return True
+# def validar_respuesta(respuesta, diccionario):
+#     if respuesta not in diccionario:
+#         return False
+#     else:
+#         return True
+#
+#
+
+print( "\n", Fore.CYAN + "Bienvenido a PyBank", end='\n\n' )
+print("Para acceder a su cuenta bancaria, ingrese su usuario y contraseña.", end='\n')
+usuario = input("Escriba su usuario: ")
+contrasena = input("Escriba su contraseña: ")
+
+# print( "\n", Fore.CYAN + "Bienvenido a su Cuenta de Banco", end='\n\n' )
+#
+# print("Usted puede hacer las siguientes operaciones:", end='\n')
+# for operacion in operaciones:
+#     print("-", operacion, end='.\n')
+#
+# operacion_Bancaria = input("\n" "Escriba la operación que realizará: ").upper()
+#
+# if not validar_respuesta(operacion_Bancaria, operaciones):
+#     print("Opcción invalida, vuélvalo a intentar...")
+#     operacion_Bancaria = input("\n" "Escriba la operación que realizará: ").upper()
+#
+# else:
 
 
-print( "\n", Fore.CYAN + "Bienvenido a su Cuenta de Banco", end='\n\n' )
-print("Usted puede hacer las siguientes operaciones:", end='\n')
-for operacion in operaciones:
-    print("-", operacion, end='.\n')
-
-operacion_Bancaria = input("\n" "Escriba la operación que realizará: ")
 
 
 
