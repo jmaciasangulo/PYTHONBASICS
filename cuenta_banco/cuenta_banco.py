1d51737f57620cc4414cfda9af8cf4afc4146627
Jesús Alejandro <jesusalejandromaciasangulo@gmail.com>
2026-05-07 21:19:43 -0600

    Se crea clase. Esta creará objetos que almacenen la información de cada operación bancaria.

---
+++ cuenta_banco/cuenta_banco.py
@@ -0,0 +1,7 @@
+class Operacion:
+    def __init__(self, fecha, hora, operacion, monto):
+        self.fecha = fecha
+        self.hora = hora
+        self.operacion = operacion
+        self.monto = monto
+        
