c6801af4b0f9ea7a81729b0c097cab0d1556aac8
Jesús Alejandro <jesusalejandromaciasangulo@gmail.com>
2026-05-17 17:29:15 -0600

    Se implementa función. Válida que el nombre de usuario exista en el archivo JSON

--- cuenta_banco/cuenta_banco.py
+++ cuenta_banco/cuenta_banco.py
@@ -73,6 +73,12 @@ def guardar_cambios():
     with open("cuentas.json", "w", encoding="utf-8") as archivo_cuentas_esc:
         json.dump(usuarios, archivo_cuentas_esc, indent=4, ensure_ascii=False)
 
+def validar_usuario(input_usuario):
+    for usuario in usuarios:
+        if usuarios[usuario]["titular"] == input_usuario:
+            return True
+    return False
+
 try:
     with open("cuentas.json", "r", encoding= "utf-8") as archivo_cuentas_lec:
         usuarios = json.load(archivo_cuentas_lec)
