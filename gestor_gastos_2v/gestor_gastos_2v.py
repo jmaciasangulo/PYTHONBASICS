44478f7fb08d3a6e44fb68f90fd6833dc526f514
Jesús Alejandro <jesusalejandromaciasangulo@gmail.com>
2026-07-04 18:01:42 -0600

    Se crea tabla 'gastos' con los campos id, que es obligatorio; nombre, descripción, monto, categoria, fecha y hora.

--- gestor_gastos_2v/gestor_gastos_2v.py
+++ gestor_gastos_2v/gestor_gastos_2v.py
@@ -2,3 +2,6 @@ import sqlite3
 
 conexion = sqlite3.connect('gastos.db')
 basedatos = conexion.cursor()
+
+basedatos.execute('''CREATE TABLE IF NOT EXISTS gastos (id TEXT PRIMARY KEY, nombre TEXT, descripccion TEXT, monto REAL,
+categoria TEXT, fecha TEXT, hora TEXT)''')
