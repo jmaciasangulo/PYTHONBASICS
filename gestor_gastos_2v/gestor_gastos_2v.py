b2b96b79296b6f24a2cc5137b76c81bc3c5502e5
Jesús Alejandro <jesusalejandromaciasangulo@gmail.com>
2026-07-06 17:10:46 -0600

    Se implementa la función que albergue el menú principal. A su ves se implementan como opcion 2 acciones

--- gestor_gastos_2v/gestor_gastos_2v.py
+++ gestor_gastos_2v/gestor_gastos_2v.py
@@ -5,3 +5,13 @@ basedatos = conexion.cursor()
 
 basedatos.execute('''CREATE TABLE IF NOT EXISTS gastos (id TEXT PRIMARY KEY, nombre TEXT, descripccion TEXT, monto REAL,
 categoria TEXT, fecha TEXT, hora TEXT)''')
+
+def menu_principal():
+     print("Bienvenido a su gestor de gastos PyRe")
+     print()
+     print("----------------------MENÚ PRINCIPAL----------------------")
+     print()
+     print("Usted puede hacer las siguientes operaciones:")
+     print("Registrar gasto----------------------------------------[1]")
+     print("Eliminar gasto-----------------------------------------[2]")
+     print()
