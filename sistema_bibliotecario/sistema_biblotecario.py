0cfad4ad8136480f8124ad00d6353d38de345e6a
Jesús Alejandro <jesusalejandromaciasangulo@gmail.com>
2026-07-21 16:21:28 -0600

    Se añaden indicaciones para que el usuario crea una contraseña segura.

--- sistema_bibliotecario/sistema_biblotecario.py
+++ sistema_bibliotecario/sistema_biblotecario.py
@@ -116,3 +116,14 @@ def validar_nombre_usuario():
         todos_caracter = all(caracter.isalpha() or caracter.isspace() for caracter in nombre_usuario)
 
     return nombre_usuario
+
+def validar_contrasena():
+    print("Es momento de crear una contraseña, por favor use:")
+    print("-Al menos 8 caracteres")
+    print("-Letras mayusculas y minusculas")
+    print("-Números")
+    print("-Caracteres especiales")
+    print()
+    while True:
+
+        contrasena = input("Ingrese su contraseña: ")
