01413cd11f77c6b0c502db3a6d9810e23a856448
Jesús Alejandro <jesusalejandromaciasangulo@gmail.com>
2026-07-25 14:20:31 -0600

    Al ingresar la contraseña se añade un intento al contador.

--- sistema_bibliotecario/sistema_biblotecario.py
+++ sistema_bibliotecario/sistema_biblotecario.py
@@ -215,6 +215,8 @@ def menu_iniciar_sesion():
     while True:
         contrasena_input = input("Ingrese su contraseña: ")
 
+        cont += 1
+
         basedatos.execute("SELECT contrasena FROM usuarios WHERE id_usuario = ?", (id_usuario_input,))
 
         if basedatos.fetchone()[0] == contrasena_input:
