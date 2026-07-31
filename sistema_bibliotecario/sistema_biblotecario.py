37ef47db31024c97579e5e63cc71971299ea7bf5
Jesús Alejandro <jesusalejandromaciasangulo@gmail.com>
2026-07-31 11:55:07 -0600

    Se confirma el material seleccionado.

--- sistema_bibliotecario/sistema_biblotecario.py
+++ sistema_bibliotecario/sistema_biblotecario.py
@@ -389,4 +389,9 @@ def menu_pedir_prestado_material(cuenta):
             id_material = input("Ingrese el ID del material que desea pedir prestado: ")
             basedatos.execute("SELEC * FROM materiales_bibliograficos WHERE id_material = ?", (id_material,))
             material_seleccionado = basedatos.fetchone()
+
+            print("MATERIAL SELECCIONADO: ")
+
+            print("ID:     ", material_seleccionado[0])
+            print("TITULO: ", material_seleccionado[2])
 menu_acceder()
